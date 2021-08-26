8b1933f75ed90784fc33391f176e0696660ad872
Sam Carey <sam@scarey.me>
2021-08-25 21:58:42 -0400

    Register sounds if they don't exist

--- src/main/java/me/scarey/musicbiomes/MusicBiomes.java
+++ src/main/java/me/scarey/musicbiomes/MusicBiomes.java
@@ -9,6 +9,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.DynamicRegistryManager;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.Biome;
 
 public class MusicBiomes implements ClientModInitializer {
 
@@ -17,25 +18,25 @@ public class MusicBiomes implements ClientModInitializer {
 
 		// Define a state wrapper than can be accessed from the lambda functions
 		var wrapper = new Object() {
-			boolean loaded = false;
 			Identifier biome = null;
 			MusicSound music = null;
 		};
 
 		// Register all biomes upon joining a game session
 		ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
-			if (!wrapper.loaded) {
-				for (Identifier biomeId : handler.getRegistryManager().get(Registry.BIOME_KEY).getIds()) {
-					SoundEvent sound = new SoundEvent(biomeToSoundEvent(biomeId));
+			Registry<SoundEvent> soundRegistry = handler.getRegistryManager().get(Registry.SOUND_EVENT_KEY);
+			for (Identifier biomeId : handler.getRegistryManager().get(Registry.BIOME_KEY).getIds()) {
+				SoundEvent sound = new SoundEvent(biomeToSoundEvent(biomeId));
+
+				// Register the sound if it doesn't exist
+				if (soundRegistry.get(sound.getId()) == null)
 					Registry.register(Registry.SOUND_EVENT, sound.getId(), sound);
-				}
-				wrapper.loaded = true;
 			}
 		});
 
 		// Register client tick listener
 		ClientTickEvents.START_CLIENT_TICK.register(client -> {
-			if(client.player != null && wrapper.loaded) {
+			if(client.player != null) {
 				DynamicRegistryManager registryManager = client.world.getRegistryManager();
 				BlockPos pos = client.player.getBlockPos();
 				Identifier biome = registryManager.get(Registry.BIOME_KEY).getId(client.world.getBiome(pos));
